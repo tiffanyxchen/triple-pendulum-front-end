c334e7f7205f98337ef10817fe1f3f56eccbb92d
tiffanyxchen <xing.t.chen@gmail.com>
2025-11-02 15:43:31 -0800

    update UserInfo.Container.tsx to display animation

--- src/pages/UserInfo/UserInfo.Container.tsx
+++ src/pages/UserInfo/UserInfo.Container.tsx
@@ -1,19 +1,89 @@
 import styled from 'styled-components'
+import { useEffect } from 'react'
+import { useErrorBoundary } from 'react-error-boundary'
+import CircularProgress from '@mui/material/CircularProgress'
+import { useSnapshot } from 'valtio'
+
+import useUserInfo from './useUserInfo'
+import Header from '../../components/Header/Header'
+import {simulationStore} from './UserInfo.State'
+import {userStore} from './UserInfo.State'
 
 const Container = styled.div`
-  background-color: 
+  border: 20px solid #808080;
   height: 100vh;
   width: 100%;
 `
 
 const UserInfo = () => {
-  // Fetch user info from the backend here
+  const {
+    orderData,
+    ordersAreLoading,
+    ordersErrors,
+    userData,
+    userIsLoading,
+    userErrors,
+  } = useUserInfo()
+
+  const orderSnap = useSnapshot(simulationStore)
+  const userSnap = useSnapshot(userStore)
+
+  const { showBoundary } = useErrorBoundary()
+
+  // Save API data into stores
+  useEffect(() => {
+    if (orderData) simulationStore.results = orderData
+  }, [orderData])
+
+  useEffect(() => {
+    if (userData) userStore.user = userData
+  }, [userData])
+
+  // Handle search bar callback
+  const onSubmitSearch = (searchText: string) => {
+    console.log("Search for:", searchText)
+    // Add filtering or navigation here later
+  }
+
+  // Top-level loading state
+  if (userIsLoading || ordersAreLoading) {
+    return (
+      <Container>
+        <CircularProgress />
+      </Container>
+    )
+  }
+
+  // Top-level errors
+  if (userErrors || ordersErrors) {
+    showBoundary(userErrors || ordersErrors)
+    return null
+  }
+
+  // If user hasn't loaded yet, avoid crash
+  if (!userSnap.user) {
+    return <Container>No user found</Container>
+  }
 
   return (
     <Container>
-      <div>
-        <div>Search bar</div>
-        <div>User notification</div>
+      <Header
+        userName={userSnap.user.name}
+        onSubmitSearch={onSubmitSearch}
+      />
+
+      <div style={{ marginTop: "2rem" }}>
+        <h2>Saved Orders (or Results)</h2>
+
+        {ordersAreLoading ? (
+          <CircularProgress color="secondary" />
+        ) : (
+          orderSnap.results?.map((order) => (
+            <div key={order.id}>
+              Order #{order.id}
+            </div>
+          ))
+        )}
       </div>
     </Container>
   )
